febcf7558ce19c90c0f7ea3014cb48dbe4de1631
Justin Winger <justinwinger4@gmail.com>
2020-03-25 13:54:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  return "Hello, my name is #{attendees}."
+  attendees.each do |name|
+  return "Hello, my name is #{name}."
+end
 end
