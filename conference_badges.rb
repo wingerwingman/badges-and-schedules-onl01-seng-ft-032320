98bebf092ad3ac968cc8dd5a13ee76633d5221f0
Justin Winger <justinwinger4@gmail.com>
2020-03-25 14:07:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,13 +13,14 @@ end
 def assign_rooms(speakers)
   list = []
   list2 = []
+  final_list = []
   rooms = [1 ,2 ,3 ,4 ,5 ,6 ,7]
   speakers.each do |name|
     list << "Hello, #{name}! " 
     rooms.each do |room_assigned|
       list2 << "You'll be assigned to room #{room_assigned}!"
     end
-    list.zip(list2)
+    final_list << list.zip(list2)
   end
-  list
+  final_list
 end
